491b59f61dbfc04bb96b48670fcc78e482d82529
subinlee147 <subin.lee147@gmail.com>
2024-03-17 14:50:01 +0900

    week2-1

---
+++ code1-1.py
@@ -0,0 +1,21 @@
+## 13 ##
+
+
+a = int('1002')#, 2)
+print(a)
+b = int('1008')#, 8)
+print(b)
+#c = int('AAFG')#, 16) --> error
+
+
+
+## 15 ##
+
+num = 12345678
+hex_num = hex(num)
+oct_num = oct(num)
+bin_num = bin(num)
+print("10진수 ==> ", num )
+print("16진수 ==> ", hex_num)
+print(" 8진수 ==> ", oct_num)
+print(" 2진수 ==> ", bin_num)
